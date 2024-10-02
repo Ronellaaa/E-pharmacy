f86ff0392ea011cd36a4b5c902f1970763c2a6c9
amanga43 <amangachandrasiri43@gmail.com>
2024-10-02 11:25:43 +0530

    did changes to the navigation-bar

--- homepage-header.php
+++ homepage-header.php
@@ -23,7 +23,7 @@ session_start();
           <ul>
             <li>
               <a href="http://localhost/E-pharmacy/home-page.php" class="link">Home</a>
-              <a href="#" class="link">About Us</a>
+              <a href="http://localhost/E-pharmacy/about-Us.php" class="link">About Us</a>
               <a href="http://localhost/E-pharmacy/project/php/product.php" class="link">Products</a>
               <a href="#" class="link">Contact Us</a>
             </li>
@@ -44,7 +44,7 @@ session_start();
                     <a href="login.php">
                         <button class="btn-login">Login</button>
                     </a>
-                    <a href="signup.php">
+                    <a href="sign-Up.php">
                         <button class="btn-login">Sign up</button>
                     </a>
                 </div>';
